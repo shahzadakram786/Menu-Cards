cdfd816247099c402156887009f5cbca663fa91f
shahzad akram <alyakram786@gmail.com>
2024-02-21 15:05:42 +0500

    removing all categori and makeing an if statemnet for it

--- script.js
+++ script.js
@@ -1,60 +1,60 @@
 const data = {
 
-    All: [{
-            img: "/images/item-1.jpeg",
-            title: "Buttermilk Pancakes",
-            price: "$15.99",
-            description: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
-        },
-        {
-            img: "/images/item-2.jpeg",
-            title: "Diner Double",
-            price: "$13.99",
-            description: "vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats",
-        },
-        {
-            img: "/images/item-3.jpeg",
-            title: "Godzilla Milkshake",
-            price: "$6.99",
-            description: "ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.",
-        },
-        {
-            img: "/images/item-4.jpeg",
-            title: "Country Delight",
-            price: "$20.99",
-            description: "Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut,",
-        },
-        {
-            img: "/images/item-5.jpeg",
-            title: "Egg Attack",
-            price: "$22.99",
-            description: "franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up",
-        },
-        {
-            img: "/images/item-6.jpeg",
-            title: "Oreo Dream",
-            price: "$18.99",
-            description: "Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday",
-        },
-        {
-            img: "/images/item-7.jpeg",
-            title: "Bacon Overflow",
-            price: "$8.99",
-            description: "carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird",
-        },
-        {
-            img: "/images/item-8.jpeg",
-            title: "American Classic",
-            price: "$12.99",
-            description: "on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut",
-        },
-        {
-            img: "/images/item-9.jpeg",
-            title: "Quarantine Buddy",
-            price: "$16.99",
-            description: "skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.",
-        },
-    ],
+    // All: [{
+    //         img: "/images/item-1.jpeg",
+    //         title: "Buttermilk Pancakes",
+    //         price: "$15.99",
+    //         description: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
+    //     },
+    //     {
+    //         img: "/images/item-2.jpeg",
+    //         title: "Diner Double",
+    //         price: "$13.99",
+    //         description: "vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats",
+    //     },
+    //     {
+    //         img: "/images/item-3.jpeg",
+    //         title: "Godzilla Milkshake",
+    //         price: "$6.99",
+    //         description: "ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.",
+    //     },
+    //     {
+    //         img: "/images/item-4.jpeg",
+    //         title: "Country Delight",
+    //         price: "$20.99",
+    //         description: "Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut,",
+    //     },
+    //     {
+    //         img: "/images/item-5.jpeg",
+    //         title: "Egg Attack",
+    //         price: "$22.99",
+    //         description: "franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up",
+    //     },
+    //     {
+    //         img: "/images/item-6.jpeg",
+    //         title: "Oreo Dream",
+    //         price: "$18.99",
+    //         description: "Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday",
+    //     },
+    //     {
+    //         img: "/images/item-7.jpeg",
+    //         title: "Bacon Overflow",
+    //         price: "$8.99",
+    //         description: "carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird",
+    //     },
+    //     {
+    //         img: "/images/item-8.jpeg",
+    //         title: "American Classic",
+    //         price: "$12.99",
+    //         description: "on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut",
+    //     },
+    //     {
+    //         img: "/images/item-9.jpeg",
+    //         title: "Quarantine Buddy",
+    //         price: "$16.99",
+    //         description: "skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.",
+    //     },
+    // ],
     Breakfast: [{
             img: "/images/item-1.jpeg",
             title: "Buttermilk Pancakes",
@@ -120,7 +120,14 @@ const data = {
 
 
 const docC = document.querySelector(".cards");
-let sir = data.All;
+
+// let sir = data.Breakfast && dat =a.lunch && data.shakes;
+// if (sir) {
+let sir = [];
+if (data.Breakfast && data.lunch && data.shakes) {
+    sir = sir.concat(data.Breakfast, data.lunch, data.shakes)
+}
+
 
 
 
@@ -146,6 +153,7 @@ for (let i = 0; i < sir.length; i++) {
     </div>
 `
 }
+// }
 
 
 const Allbutn = document.getElementById("All");
@@ -155,10 +163,11 @@ Allbutn.addEventListener('click', (e) => {
 
     docC.innerHTML = "";
 
+    if (sir) {
 
-    for (let i = 0; i < sir.length; i++) {
-        docC.innerHTML +=
-            `
+        for (let i = 0; i < sir.length; i++) {
+            docC.innerHTML +=
+                `
         <div class = "cards_main">
         <div class = "image">
         <img  src= "${sir[i].img}" class="img">
@@ -177,8 +186,9 @@ Allbutn.addEventListener('click', (e) => {
         </div>
     `
 
+        }
+        console.log("inside if statemen  : = > :", docC)
     }
-
 
 })
 
