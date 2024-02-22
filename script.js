be9b3f25586ae8a6f40d4f410922c1805f857cff
shahzad akram <alyakram786@gmail.com>
2024-02-22 21:59:42 +0500

    breakfast butn just worked

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-const data = [
-
-
-    {
+const data = [{
         category: "Breakfast",
         img: "/images/item-1.jpeg",
         title: "Buttermilk Pancakes",
         price: "$15.99",
         description: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
-    }, {
+    },
+    {
         category: "Breakfast",
         img: "/images/item-4.jpeg",
         title: "Country Delight",
         price: "$20.99",
         description: "Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut,",
-    }, {
+    },
+    {
         category: "Breakfast",
 
         img: "/images/item-7.jpeg",
@@ -29,70 +28,66 @@ const data = [
         title: "Diner Double",
         price: "$13.99",
         description: "vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats",
-    }, {
+    },
+    {
         category: "Lunch",
         img: "/images/item-5.jpeg",
         title: "Egg Attack",
         price: "$22.99",
         description: "franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up",
-    }, {
+    },
+    {
         category: "Lunch",
         img: "/images/item-8.jpeg",
         title: "American Classic",
         price: "$12.99",
         description: "on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut",
-    }, {
+    },
+    {
         category: "Shakes",
         img: "/images/item-3.jpeg",
         title: "Godzilla Milkshake",
         price: "$6.99",
         description: "ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.",
-    }, {
+    },
+    {
         category: "Shakes",
         img: "/images/item-6.jpeg",
         title: "Oreo Dream",
         price: "$18.99",
         description: "Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday",
-    }, {
+    },
+    {
         category: "Shakes",
         img: "/images/item-9.jpeg",
         title: "Quarantine Buddy",
         price: "$16.99",
         description: "skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.",
     },
-
-]
-
+];
 
 const docC = document.querySelector(".cards");
-const buttons = document.querySelector(".buttons")
+const buttons = document.getElementsByClassName("buttons");
+console.log(buttons);
 
 for (let i = 0; i < data.length; i++) {
-
-
-    buttons.addEventListener("click", (e) => {
+    buttons[i].addEventListener("click", (e) => {
         console.log("hello there felling well today ");
         console.log(data[0].category);
-        if (e.target === data[0].category) {
 
+        console.log(e.target.innerHTML);
+        if (e.target.innerHTML === data[i].category) {
+            console.log("inside");
+            docC.innerHTML += data[i].category;
+
+            dataStructure(data[i].category)
 
         }
 
-
-
-
-
-
-
-
-    })
-
-
+    });
 
     function dataStructure() {
-
-        docC.innerHTML +=
-            `
+        docC.innerHTML += `
     <div class = "cards_main">
     <div class = "image">
     <img  src= "${data[i].img}" class="img">
@@ -109,46 +104,30 @@ for (let i = 0; i < data.length; i++) {
     </div>
 
     </div>
-`
+`;
+
     }
     dataStructure();
-
 }
 
+// making for functoin
 
+// function All() {
+//     console.log("hi am all  button ");
+// }
 
+// function Breakfast() {
+//     console.log("hi  i am the button of breakfast");
+// }
 
+// function Lunch() {
+//     console.log("hi am the lunch button");
+// }
 
-
-
-
-// making for functoin  
-
-
-
-function All() {
-    console.log("hi am all  button ");
-}
-
-
-function Breakfast() {
-
-    console.log("hi  i am the button of breakfast");
-}
-
-
-function Lunch() {
-
-    console.log("hi am the lunch button");
-
-}
-
-
-function Shakes() {
-    console.log("hi i am the shakes");
-}
+// function Shakes() {
+//     console.log("hi i am the shakes");
+// }
 // const data = {
-
 
 //     Breakfast: [{
 //             img: "/images/item-1.jpeg",
@@ -210,22 +189,12 @@ function Shakes() {
 //     ],
 // }
 
-
-
-
-
-
 // const docC = document.querySelector(".cards");
-
 
 // let sir = [];
 // if (data.Breakfast && data.lunch && data.shakes) {
 //     sir = sir.concat(data.Breakfast, data.lunch, data.shakes)
 // }
-
-
-
-
 
 // for (let i = 0; i < sir.length; i++) {
 //     docC.innerHTML +=
@@ -242,18 +211,15 @@ function Shakes() {
 //     </div>
 //     <p id="desc"> ${sir[i].description}</p>
 
-
 //     </div>
 
 //     </div>
 // `
 // }
 
-
 // const Allbutn = document.getElementById("All");
 
 // Allbutn.addEventListener('click', (e) => {
-
 
 //     docC.innerHTML = "";
 
@@ -274,7 +240,6 @@ function Shakes() {
 //         </div>
 //         <p id="desc"> ${sir[i].description}</p>
 
-
 //         </div>
 
 //         </div>
@@ -286,17 +251,10 @@ function Shakes() {
 
 // })
 
-
 // const Breakfastbutn = document.getElementById("Break");
-
-
-
-
 
 // Breakfastbutn.addEventListener('click', () => {
 //     docC.innerHTML = "";
-
-
 
 //     const sir2 = data.Breakfast;
 
@@ -315,7 +273,6 @@ function Shakes() {
 //         </div>
 //         <p id="desc"> ${sir2[i].description}</p>
 
-
 //         </div>
 
 //         </div>
@@ -323,17 +280,10 @@ function Shakes() {
 //     }
 // })
 
-
-
 // const lunchbutn = document.getElementById("Lunch")
-
-
-
 
 // lunchbutn.addEventListener('click', () => {
 //     docC.innerHTML = "";
-
-
 
 //     const sir3 = data.lunch;
 
@@ -352,7 +302,6 @@ function Shakes() {
 //         </div>
 //         <p id="desc"> ${sir3[i].description}</p>
 
-
 //         </div>
 
 //         </div>
@@ -360,20 +309,10 @@ function Shakes() {
 //     }
 // })
 
-
-
-
-
-
 // const shakesbutn = document.getElementById("Dinner")
-
-
-
 
 // shakesbutn.addEventListener('click', () => {
 //     docC.innerHTML = "";
-
-
 
 //     const sir4 = data.shakes;
 
@@ -391,7 +330,6 @@ function Shakes() {
 //         <p id="price">${sir4[i].price}</p>
 //         </div>
 //         <p id="desc"> ${sir4[i].description}</p>
-
 
 //         </div>
 
