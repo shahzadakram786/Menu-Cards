d294fdf227185453295f254d67fca827896fa0e1
shahzad akram <alyakram786@gmail.com>
2024-02-22 21:06:01 +0500

    starting the buttons part

--- script.js
+++ script.js
@@ -2,16 +2,20 @@ const data = [
 
 
     {
+        category: "Breakfast",
         img: "/images/item-1.jpeg",
         title: "Buttermilk Pancakes",
         price: "$15.99",
         description: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
     }, {
+        category: "Breakfast",
         img: "/images/item-4.jpeg",
         title: "Country Delight",
         price: "$20.99",
         description: "Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut,",
     }, {
+        category: "Breakfast",
+
         img: "/images/item-7.jpeg",
         title: "Bacon Overflow",
         price: "$8.99",
@@ -19,31 +23,38 @@ const data = [
     },
 
     {
+        category: "Lunch",
+
         img: "/images/item-2.jpeg",
         title: "Diner Double",
         price: "$13.99",
         description: "vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats",
     }, {
+        category: "Lunch",
         img: "/images/item-5.jpeg",
         title: "Egg Attack",
         price: "$22.99",
         description: "franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up",
     }, {
+        category: "Lunch",
         img: "/images/item-8.jpeg",
         title: "American Classic",
         price: "$12.99",
         description: "on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut",
     }, {
+        category: "Shakes",
         img: "/images/item-3.jpeg",
         title: "Godzilla Milkshake",
         price: "$6.99",
         description: "ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.",
     }, {
+        category: "Shakes",
         img: "/images/item-6.jpeg",
         title: "Oreo Dream",
         price: "$18.99",
         description: "Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday",
     }, {
+        category: "Shakes",
         img: "/images/item-9.jpeg",
         title: "Quarantine Buddy",
         price: "$16.99",
@@ -54,9 +65,27 @@ const data = [
 
 
 const docC = document.querySelector(".cards");
-
+const buttons = document.querySelector(".buttons")
 
 for (let i = 0; i < data.length; i++) {
+
+
+    buttons[i].addEventListener("click", () => {
+        console.log("hello there felling well today ");
+
+
+
+
+
+
+
+
+    })
+
+
+
+
+
     docC.innerHTML +=
         `
     <div class = "cards_main">
